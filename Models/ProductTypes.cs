a2090346be0d978a25b354e767f0545c4565d5b2
Lindos-Mahadi <lindosmahadi@.com>
2021-09-13 20:09:48 +0600

    Created All ClassTable

---
+++ Models/ProductTypes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingStore.Models
+{
+    public class ProductTypes
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Product Type")]
+        public string ProductType { get; set; }
+    }
+}
---
+++ Models/SpecialTag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingStore.Models
+{
+    public class SpecialTag
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Tag Name")]
+        public string SpecialTagName { get; set; }
+    }
+}
