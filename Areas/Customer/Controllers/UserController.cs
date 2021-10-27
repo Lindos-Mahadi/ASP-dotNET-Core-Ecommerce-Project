649f12d4a1a8d2861123bdd17c13877a08ab61ed
Lindos-Mahadi <lindosmahadi@.com>
2021-10-27 20:43:02 +0600

    Created User Controller & User Get and Post Method & User Index & PartialView for User

---
+++ Areas/Customer/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingStore.Data;
+using ShoppingStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingStore.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class UserController : Controller
+    {
+        UserManager<IdentityUser> _userManager;
+        ApplicationDbContext _db;
+        public UserController(UserManager<IdentityUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ApplicationUsers.ToList());
+        }
+
+        public async Task<IActionResult> Create()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userManager.CreateAsync(user, user.PasswordHash);
+                if (result.Succeeded)
+                {
+                    //var isSaveRole = await _userManager.AddToRoleAsync(user, role: "User");
+                    TempData["save"] = "User has been Created Successfully";
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View();
+        }
+    }
+}
