13337f09f71bb7f0bc1a78e21e2f6519964e1828
Lindos-Mahadi <lindosmahadi@.com>
2021-10-29 19:51:43 +0600

    Role Create Method is Created

--- Areas/Admin/Controllers/RoleController.cs
+++ Areas/Admin/Controllers/RoleController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ShoppingStore.Data;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ShoppingStore.Areas.Admin.Controllers
 {
@@ -18,5 +23,38 @@ namespace ShoppingStore.Areas.Admin.Controllers
             ViewBag.Roles = roles;
             return View();
         }
+
+        // Get action Create Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST action Create Method
+        [HttpPost]
+        public async Task <IActionResult> Create(string name)
+        {
+
+            IdentityRole role = new IdentityRole();
+            role.Name = name;
+            var isExist = await _roleManager.RoleExistsAsync(role.Name);
+
+            if (isExist)
+            {
+                ViewBag.msg = "This Role is already exist";
+                ViewBag.name = name;
+                return View();
+            }
+            var result = await _roleManager.CreateAsync(role);
+
+            if (result.Succeeded)
+            {
+                TempData["save"] = "Role has been saved Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        
     }
 }
