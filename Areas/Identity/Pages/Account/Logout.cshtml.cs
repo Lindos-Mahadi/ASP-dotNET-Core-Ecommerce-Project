86fc001b75acd5951dcb207b8b7b3d5ef0550239
Lindos-Mahadi <lindosmahadi@.com>
2021-10-30 20:26:55 +0600

    Role Based Menu

--- Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -25,6 +26,7 @@ namespace ShoppingStore.Areas.Identity.Pages.Account
         public void OnGet()
         {
         }
+
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
